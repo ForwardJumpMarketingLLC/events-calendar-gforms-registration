52c46a8ba9353bfd4b8f4698e157979bacf62b29
Timothy Jensen <tim@timjensen.us>
2017-07-23 09:38:42 -0400

    Initial commit.

---
+++ events-calendar-gf-registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Events Calendar Gf Registration
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     events-calendar-gf-registration
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Events_Calendar_Gf_Registration
+ */
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Events_Calendar_Gf_Registration
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
