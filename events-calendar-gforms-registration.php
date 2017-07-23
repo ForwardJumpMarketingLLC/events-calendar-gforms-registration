fa76474b6bece2ebadda249b8054b0a9c771ea7e
Timothy Jensen <tim@timjensen.us>
2017-07-23 09:49:08 -0400

    Changed plugin slug and updated header information.

---
+++ events-calendar-gforms-registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Events Calendar GForms Registration
+ * Plugin URI:      https://github.com/ForwardJumpMarketingLLC/events-calendar-gforms-registration
+ * Description:     Use Gravity Forms to handle registration for The Events Calendar events.
+ * Author:          Tim Jensen
+ * Author URI:      https://forwardjump.com
+ * Text Domain:     events-calendar-gforms-registration
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         ForwardJump\Events_Calendar_GForms_Registration
+ */
