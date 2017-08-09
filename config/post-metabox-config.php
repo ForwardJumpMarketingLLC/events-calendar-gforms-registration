636ffef410461a4d577206b7a62b490e22b1a4e6
Timothy Jensen <tim@timjensen.us>
2017-08-09 14:54:10 -0400

    Cleaned up config.

--- config/post-metabox-config.php
+++ config/post-metabox-config.php
@@ -2,36 +2,34 @@
 /**
  * Post metaboxes.
  *
- * @package ForwardJump\EC_GF_Registration
+ * @package ForwardJump\ECGF_Registration
  * @since   0.1.0
  * @author  Tim Jensen
  * @link    https://forwardjump.com/
  * @license GNU General Public License 2.0+
  */
 
-namespace ForwardJump\EC_GF_Registration;
+namespace ForwardJump\ECGF_Registration;
 
 /**
  * This is an example config array.
  *
- * 'post_type'      (string) => Post type slug.
- * 'metabox_title'  (string) => Title of the metabox.
- * 'metabox_fields' (array)  => The CMB2 fields that will display within the
- *                              metabox.
+ * 'metabox' (string) => Title of the metabox.
+ * 'fields'  (array)  => The CMB2 fields that will display within the metabox.
  */
 return [
 	[
 		'metabox' => [
 			'object_types' => [ 'tribe_events' ],
 			'title'        => 'Event Registration Information',
-			'show_on_cb'   => 'ForwardJump\EC_GF_Registration\is_gf_active',
+			'show_on_cb'   => 'ForwardJump\ECGF_Registration\is_gf_active',
 		],
 		'fields'  => [
 			[
 				'name'       => 'Select Registration Form',
 				'id'         => 'ecgf_selected_form_id',
 				'type'       => 'select',
-				'options_cb' => 'ForwardJump\EC_GF_Registration\get_forms',
+				'options_cb' => 'ForwardJump\ECGF_Registration\get_forms',
 			],
 			[
 				'name'        => 'Notice',
@@ -40,16 +38,16 @@ return [
 				'type'        => 'textarea_small',
 			],
 			[
-				'type'   => 'group',
-				'id'     => 'ecgf_form_fields',
-				'options'     => array(
-					'group_title'   => __( 'Form Field - reference as {field_{#}}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
-					'add_button'    => __( 'Add Another Field', 'cmb2' ),
+				'type'    => 'group',
+				'id'      => 'ecgf_form_settings',
+				'options' => array(
+					'group_title' => __( 'Form Field - reference as {field_{#}}', ECGF_DIR_TEXT_DOMAIN ),
+					'add_button'  => __( 'Add Another Field', ECGF_DIR_TEXT_DOMAIN ),
 				),
-				'fields' => [
+				'fields'  => [
 					[
-						'name' => 'Maximum Registrants',
-						'id'   => 'max_participants',
+						'name' => 'Maximum Reservations',
+						'id'   => 'max_reservations',
 						'type' => 'text_small',
 					],
 					[
@@ -57,7 +55,7 @@ return [
 						'description' => 'Choose the form field that should be used to update the number of registrants.',
 						'id'          => 'field_id',
 						'type'        => 'select',
-						'options_cb'     => 'ForwardJump\EC_GF_Registration\get_form_fields',
+						'options_cb'  => 'ForwardJump\ECGF_Registration\get_form_fields',
 					],
 				],
 			],
